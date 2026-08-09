b542f229f766865f6ea476e236aac696b2ae5951
anjelogigi <anjelogigi@gmail.com>
2026-08-09 12:55:25 +0530

    Create test_pkg.sv

---
+++ src/tb/test_pkg.sv
@@ -0,0 +1,20 @@
+package test_pkg;
+
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+  `include "defines.svh"
+  `include "sequence_item.sv"
+  `include "alu_cfg.sv"
+  `include "alu_subscriber.sv"
+  `include "driver.sv"
+  `include "input_monitor.sv"
+  `include "sequencer.sv"
+  `include "input_agent.sv"
+  `include "output_monitor.sv"
+  `include "output_agent.sv"
+  `include "scoreboard.sv"
+  `include "env.sv"
+  `include "sequence.sv"
+  `include "test.sv"
+
+endpackage
