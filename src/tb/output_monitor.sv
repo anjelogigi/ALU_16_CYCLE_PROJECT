8a542a0d88bd85b1a0acaa6181ee0b69fbab7422
anjelogigi <anjelogigi@gmail.com>
2026-08-09 12:46:23 +0530

    Create output_monitor.sv

---
+++ src/tb/output_monitor.sv
@@ -0,0 +1,69 @@
+class output_monitor extends uvm_monitor;
+
+  `uvm_component_utils(output_monitor)
+
+  uvm_analysis_port #(trans) out_monitor_port;
+
+  virtual alu_if.OUT_MON vif;
+  alu_config m_cfg;
+
+  trans rd_data;
+
+  function new(string name="output_monitor", uvm_component parent);
+    super.new(name, parent);
+  endfunction
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+
+    if(!uvm_config_db#(alu_config)::get(this, "", "alu_config", m_cfg))
+      `uvm_fatal(get_type_name(), "Output Monitor Getting Failed")
+
+    out_monitor_port = new("out_monitor_port", this);
+
+  endfunction
+
+  function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    vif = m_cfg.vif;
+  endfunction
+
+  task run_phase(uvm_phase phase);
+
+    forever
+    begin
+
+      rd_data = trans::type_id::create("rd_data");
+
+      collect_data();
+
+      `uvm_info("OUTPUT_MONITOR", $sformatf("OUTPUT MONITOR\n%s", rd_data.sprint()), UVM_LOW)
+
+      out_monitor_port.write(rd_data);
+
+    end
+
+  endtask
+
+  virtual task collect_data();
+
+    @(vif.out_mon_cb);
+
+    rd_data.res       = vif.out_mon_cb.res;
+    rd_data.err       = vif.out_mon_cb.err;
+    rd_data.cout      = vif.out_mon_cb.cout;
+    rd_data.oflow     = vif.out_mon_cb.oflow;
+    rd_data.G         = vif.out_mon_cb.G;
+    rd_data.E         = vif.out_mon_cb.E;
+    rd_data.L         = vif.out_mon_cb.L;
+    rd_data.ce        = vif.out_mon_cb.ce;
+    rd_data.mode      = vif.out_mon_cb.mode;
+    rd_data.cin       = vif.out_mon_cb.cin;
+    rd_data.cmd       = vif.out_mon_cb.cmd;
+    rd_data.inp_valid = vif.out_mon_cb.inp_valid;
+    rd_data.OA        = vif.out_mon_cb.OA;
+    rd_data.OB        = vif.out_mon_cb.OB;
+
+  endtask
+
+endclass
